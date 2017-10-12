6e851a6a762063149a3c3961bacc324fa4367c3a
RookieDouby <zhd1109@qq.com>
2017-10-12 10:23:22 +0800

    set up server

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/**
+ * Created by zhanghongdou on 2017/10/11.
+ */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: `${__dirname}/app/index.html`,
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: [
+        './app/index.js',
+    ],
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'index_bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                loader: 'babel-loader',
+                // query: {
+                //     presets: ['es2015','react']
+                // },
+            }
+        ]
+    },
+    //webpack-dev-server 设定
+    devServer: {
+        inline: true,
+        port: 8008
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
